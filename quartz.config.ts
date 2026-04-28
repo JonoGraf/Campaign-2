92b44fe1d295b1e7662d8fae7087457f4a4c4639
JonoGraf <jonathongraf@gmail.com>
2026-04-28 12:45:06 +1000

    Quartz sync: Apr 28, 2026, 12:45 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "Campaign 2",
+    pageTitleSuffix: "Not Cantrips",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
